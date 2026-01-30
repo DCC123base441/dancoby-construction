e59596e3066c90c828330318499a21e108618355
info <info@dancoby.com>
2026-01-30 08:35:31 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ChatBot from './components/ChatBot';
 
 export default function Layout({ children }) {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
