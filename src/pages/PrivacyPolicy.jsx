1ddc1e06b21047714284e4314dc23b175721e6fb
info <info@dancoby.com>
2026-01-30 02:53:19 +0000

    File changes

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -37,7 +37,7 @@ export default function PrivacyPolicy() {
       const documentHeight = document.documentElement.scrollHeight;
 
       // Check if we're at the very bottom of the page
-      if (window.scrollY + windowHeight >= documentHeight - 10) {
+      if (window.scrollY + windowHeight >= documentHeight - 2) {
         setActiveSection('contact');
         return;
       }
