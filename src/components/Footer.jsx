4f04f7340642ede3ca83de3aebcac97bdd3df30e
info <info@dancoby.com>
2026-01-31 19:17:25 +0000

    File changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -161,6 +161,9 @@ export default function Footer() {
             <Link to={createPageUrl('TermsOfService')} className="hover:text-white transition-colors">
               Terms of Service
             </Link>
+            <Link to={createPageUrl('AdminDashboard')} className="hover:text-white transition-colors opacity-50 hover:opacity-100">
+              Admin
+            </Link>
           </div>
         </div>
       </div>
