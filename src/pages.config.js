4be7ade953515fed32637e93a3f3561de1e862b1
info <info@dancoby.com>
2026-02-01 02:49:37 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -48,17 +48,15 @@
  * The mainPage value must match a key in the PAGES object exactly.
  */
 import About from './pages/About';
+import AdminAnalytics from './pages/AdminAnalytics';
 import AdminBlog from './pages/AdminBlog';
-import AdminDashboard from './pages/AdminDashboard';
 import AdminEstimates from './pages/AdminEstimates';
 import AdminLeads from './pages/AdminLeads';
-import AdminLogin from './pages/AdminLogin';
 import AdminProjects from './pages/AdminProjects';
 import Contact from './pages/Contact';
 import Estimator from './pages/Estimator';
 import Hiring from './pages/Hiring';
 import HiringApplication from './pages/HiringApplication';
-import Home from './pages/Home';
 import Press from './pages/Press';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import ProjectDetail from './pages/ProjectDetail';
@@ -70,23 +68,23 @@ import ServiceTownhouses from './pages/ServiceTownhouses';
 import Services from './pages/Services';
 import TermsOfService from './pages/TermsOfService';
 import VendorIntake from './pages/VendorIntake';
-import AdminAnalytics from './pages/AdminAnalytics';
+import Home from './pages/Home';
+import AdminDashboard from './pages/AdminDashboard';
+import AdminLogin from './pages/AdminLogin';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "About": About,
+    "AdminAnalytics": AdminAnalytics,
     "AdminBlog": AdminBlog,
-    "AdminDashboard": AdminDashboard,
     "AdminEstimates": AdminEstimates,
     "AdminLeads": AdminLeads,
-    "AdminLogin": AdminLogin,
     "AdminProjects": AdminProjects,
     "Contact": Contact,
     "Estimator": Estimator,
     "Hiring": Hiring,
     "HiringApplication": HiringApplication,
-    "Home": Home,
     "Press": Press,
     "PrivacyPolicy": PrivacyPolicy,
     "ProjectDetail": ProjectDetail,
@@ -98,7 +96,9 @@ export const PAGES = {
     "Services": Services,
     "TermsOfService": TermsOfService,
     "VendorIntake": VendorIntake,
-    "AdminAnalytics": AdminAnalytics,
+    "Home": Home,
+    "AdminDashboard": AdminDashboard,
+    "AdminLogin": AdminLogin,
 }
 
 export const pagesConfig = {
