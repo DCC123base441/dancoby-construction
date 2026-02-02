984b294e61f84c22e5c97f9b98cf7d0f1613ac4a
Ralph Abekassis <info@dancoby.com>
2026-02-02 04:17:53 +0000

    File changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Mail, MapPin, Instagram, Facebook } from 'lucide-react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { createPageUrl } from '../utils';
 
 export default function Footer() {
