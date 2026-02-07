b53d6d12d3444a16fbc0b6d8c4c6bf1ca3a1fd2b
Ralph Abekassis <info@dancoby.com>
2026-02-07 04:27:23 +0000

    File changes

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -283,7 +283,7 @@ export default function ChatBot() {
             </div>
 
             {/* Messages */}
-            <div className="flex-1 overflow-y-auto p-4 space-y-4">
+            <div className="flex-1 overflow-y-auto p-4 space-y-4 overscroll-contain" style={{ touchAction: 'pan-y' }}>
               {messages.map((m, idx) => (
                 <div
                   key={idx}
