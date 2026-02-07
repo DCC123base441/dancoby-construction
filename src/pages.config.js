abaee03d26e7c41b3eb8fb63f994a6a3353430c4
Ralph Abekassis <info@dancoby.com>
2026-02-07 20:50:25 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -66,7 +66,6 @@ import Contact from './pages/Contact';
 import Estimator from './pages/Estimator';
 import Hiring from './pages/Hiring';
 import HiringApplication from './pages/HiringApplication';
-import Home from './pages/Home';
 import Press from './pages/Press';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import ProjectDetail from './pages/ProjectDetail';
@@ -84,6 +83,7 @@ import StepSuccess from './pages/StepSuccess';
 import StepSummary from './pages/StepSummary';
 import TermsOfService from './pages/TermsOfService';
 import VendorIntake from './pages/VendorIntake';
+import Home from './pages/Home';
 import __Layout from './Layout.jsx';
 
 
@@ -107,7 +107,6 @@ export const PAGES = {
     "Estimator": Estimator,
     "Hiring": Hiring,
     "HiringApplication": HiringApplication,
-    "Home": Home,
     "Press": Press,
     "PrivacyPolicy": PrivacyPolicy,
     "ProjectDetail": ProjectDetail,
@@ -125,6 +124,7 @@ export const PAGES = {
     "StepSummary": StepSummary,
     "TermsOfService": TermsOfService,
     "VendorIntake": VendorIntake,
+    "Home": Home,
 }
 
 export const pagesConfig = {
