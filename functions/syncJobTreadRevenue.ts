e537d93a6540117651ce1cf5fa8c4335d8c326b4
Ralph Abekassis <info@dancoby.com>
2026-02-09 20:09:21 +0000

    File changes

--- functions/syncJobTreadRevenue.ts
+++ functions/syncJobTreadRevenue.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.11';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 import moment from 'npm:moment';
 
 export default async function handler(req) {
