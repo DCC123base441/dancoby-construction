edc2947b642824487d9f48cff2ed8bf65b537853
Ralph Abekassis <info@dancoby.com>
2026-02-13 03:03:14 +0000

    Change font to Roboto

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -72,16 +72,14 @@ export default function Layout({ children, currentPageName }) {
 
   if (isPortal || isAdmin) {
     return (
-      <div className="min-h-screen bg-gray-50" style={{ fontFamily: "'Roboto', sans-serif" }}>
-        <style>{`@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');`}</style>
+      <div className="min-h-screen bg-gray-50">
         {children}
       </div>
     );
   }
 
   return (
-    <div className="min-h-screen flex flex-col" style={{ fontFamily: "'Roboto', sans-serif" }}>
-      <style>{`@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');`}</style>
+    <div className="min-h-screen flex flex-col">
       <ScrollProgress />
       <Header />
       <main className="flex-1 pt-20">
